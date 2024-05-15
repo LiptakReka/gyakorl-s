19a11aee50f8277f342b3b30390a285ceb8450ce
LiptakReka <liptakr@kkszki.hu>
2024-05-15 14:30:49 +0200

    új fileok

---
+++ indexTest.py
@@ -0,0 +1,15 @@
+# imports
+
+# set driver
+
+# functions
+
+# test1()
+# time.sleep(0.1)
+# test2()
+# time.sleep(0.1)
+# test3()
+# time.sleep(0.1)
+# test4()
+# time.sleep(0.1)
+# test5()
---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // element-one: Hozzáad egy "box1style" class-t kattintáskor
+    const elementOne = document.getElementById('element-one');
+
+    // element-two: Dupla kattintásra hozzáad egy 2px-es, fekete keretet
+    const elementTwo = document.getElementById('element-two');
+
+    // element-three: Ha rámegy az egér, eltűnik
+    const elementThree = document.getElementById('element-three');
+
+    // element-four: Kattintásra a szövegszín piros lesz benne
+    const elementFour = document.getElementById('element-four');
+
+    // Input mező: Billentyű lenyomásra az inputmező háttérszíne szürke lesz
+    const inputField = document.getElementById('myInput');
+    
+});
